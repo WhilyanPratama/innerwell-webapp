5bd58b433d8c1eb7bf0388aa0cb9c35f3ac1ef45
Whilyan Pratama <166719174+WhilyanPratama@users.noreply.github.com>
2025-06-21 12:16:01 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->prepend(\App\Http\Middleware\ForceHttps::class);
         $middleware->validateCsrfTokens(except: [
             'midtrans/callback' 
         ]);
